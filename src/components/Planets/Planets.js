d99089712f7ae07a82eaff1751ea810635c6dc2f
miliii1 <milagrosjara26@gmail.com>
2021-10-19 17:32:51 -0300

    falta css naves

--- src/components/Planets/Planets.js
+++ src/components/Planets/Planets.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa'
+import Nav from '../../components/Nav/Nav'
+import './planets.css'
 
-function Planets() {
+
+const Planets = () => {
+    const [page, setPage] = useState(1)
+    const [countPlanetas, setCountPlanetas] = useState(0)
+    const [planetas, setPlanetas] = useState([])
+    const [habitantes, setHabitantes] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/planets/?page=${page}`).then(response => {
+            const contagem = response.data.count
+            const planetas = response.data.results
+
+            setCountPlanetas(contagem)
+            setPlanetas(planetas)
+
+            response.data.next === null ?
+                document.getElementById('btnNext').style.display = 'none' :
+                document.getElementById('btnNext').style.display = 'block'
+
+            response.data.previous === null ?
+                document.getElementById('btnPrevious').style.display = 'none' :
+                document.getElementById('btnPrevious').style.display = 'block'
+        })
+    }, [page])
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/people/?page=${page}`).then(response => {
+            const pessoas = response.data.results
+            const nomePessoa = pessoas.map(pessoa => pessoa.name)
+            setHabitantes(nomePessoa)
+        })
+    }, [page])
+
+    function handleNextPage() {
+        const currentPage = page + 1
+        setPage(currentPage)
+    }
+
+    function handlePreviousPage() {
+        const currentPage = page - 1
+        setPage(currentPage)
+    }
     return (
-        <div>
-            <h1>PLANETAS</h1>
+        <div id='main'>
+            <Nav />
+
+            <div className='count'>
+                <h1>TOTAL PLANETS: {countPlanetas}</h1>
+            </div>
+
+            <div id='content'>
+                {planetas.map(planetas => (
+                    <div className='card-planetas' key={planetas.name}>
+                        <h2>{planetas.name}</h2>
+                        <span>Rotation Period: {planetas.rotation_period}</span>
+                        <span>Orbital Period: {planetas.orbital_period}</span>
+                        <span>Diameter: {planetas.diameter}</span>
+                        <span>Climate: {planetas.climate}</span>
+                        <span>Gravity: {planetas.gravity}</span>
+                        <span>Terrain: {planetas.terrain}</span>
+                        <span>Surface Water: {planetas.surface_water}</span>
+                        <span>Population: {planetas.population}</span>
+                        <span>Residents: {
+                            habitantes[String(planetas.residents[0]).substr(28, 3).replace('/', '') - 1]
+                        }</span>
+                    </div>
+                ))}
+            </div>
+
+            <div className='navigation-page'>
+                <button id='btnPrevious' onClick={handlePreviousPage}><FaArrowCircleLeft />PREVIOUS PAGE</button>
+
+                <button id='btnNext' onClick={handleNextPage}>NEXT PAGE<FaArrowCircleRight /></button>
+            </div>
         </div>
     )
 }
--- src/components/StarShips/Ships.js
+++ src/components/StarShips/Ships.js
@@ -1,9 +1,87 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import { FaArrowCircleLeft, FaArrowCircleRight } from 'react-icons/fa'
+import Nav from '../../components/Nav/Nav'
+import './ships.css'
 
-function Ships() {
+
+const Ships = () => {
+    const [page, setPage] = useState(1)
+    const [countEspacoNaves, setCountEspacoNaves] = useState(0)
+    const [espacoNaves, setEspacoNaves] = useState([])
+    const [pilotos, setPilotos] = useState([])
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/starships/?page=${page}`).then(response => {
+            const contagem = response.data.count
+            const naves = response.data.results
+
+            setCountEspacoNaves(contagem)
+            setEspacoNaves(naves)
+
+            response.data.next === null ?
+                document.getElementById('btnNext').style.display = 'none' :
+                document.getElementById('btnNext').style.display = 'block'
+
+            response.data.previous === null ?
+                document.getElementById('btnPrevious').style.display = 'none' :
+                document.getElementById('btnPrevious').style.display = 'block'
+        })
+    }, [page])
+
+    useEffect(() => {
+        axios.get(`https://swapi.dev/api/people/?page=${page}`).then(response => {
+            const pilotos = response.data.results
+            const nomePilotos = pilotos.map(piloto => piloto.name)
+            setPilotos(nomePilotos)
+        })
+    }, [page])
+
+    function handleNextPage() {
+        const currentPage = page + 1
+        setPage(currentPage)
+    }
+
+    function handlePreviousPage() {
+        const currentPage = page - 1
+        setPage(currentPage)
+    }
+
     return (
-        <div>
-            <h1>NAVES STAR</h1>
+        <div id='main'>
+            <Nav />
+
+            <div className='count'>
+                <h1>TOTAL STARSHIPS: {countEspacoNaves}</h1>
+            </div>
+
+            <div id='content'>
+                {espacoNaves.map(espacoNaves => (
+                    <div className='card-naves' key={espacoNaves.name}>
+                        <h2>{espacoNaves.name}</h2>
+                        <span>Model: {espacoNaves.model}</span>
+                        <span>Manufacturer: {espacoNaves.manufacturer}</span>
+                        <span>Cost in Credits: {espacoNaves.cost_in_credits}</span>
+                        <span>Length: {espacoNaves.length}</span>
+                        <span>max_atmosphering_speed: {espacoNaves.max_atmosphering_speed}</span>
+                        <span>Crew: {espacoNaves.crew}</span>
+                        <span>Passengers: {espacoNaves.passengers}</span>
+                        <span>Cargo Capacity: {espacoNaves.cargo_capacity}</span>
+                        <span>Consumables: {espacoNaves.consumables}</span>
+                        <span>Hyperdrive Rating: {espacoNaves.hyperdrive_rating}</span>
+                        <span>MGLT: {espacoNaves.MGLT}</span>
+                        <span>Starship Class: {espacoNaves.starship_class}</span>
+                        <span>Pilots: {
+                            pilotos[String(espacoNaves.pilots[0]).substr(28, 3).replace('/', '') - 1]
+                        }</span>
+                    </div>
+                ))}
+            </div>
+            <div className='navigation-page'>
+                <button id='btnPrevious' onClick={handlePreviousPage}><FaArrowCircleLeft />PREVIOUS PAGE</button>
+
+                <button id='btnNext' onClick={handleNextPage}>NEXT PAGE<FaArrowCircleRight /></button>
+            </div>
         </div>
     )
 }
